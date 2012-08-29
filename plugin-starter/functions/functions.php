c083922ceeead727d62ed603232f06220076bbc0
martythornley <martythornley@gmail.com>
2012-08-28 20:06:20 -0700

    Added idea for registering dev networks, just need to figure out how to load them in right way.

--- plugin-starter/functions/functions.php
+++ plugin-starter/functions/functions.php
@@ -1,16 +1,23 @@
 <?php
 
-	if ( !function_exists( 'dev_debug' ) ) {
-		function dev_debug( $data ) {
-			print '<pre>'; print_r( $data ); print '</pre>';
-		}
+if ( !function_exists( 'dev_debug' ) ) {
+	function dev_debug( $data ) {
+		print '<pre>'; print_r( $data ); print '</pre>';
 	}
+}
 
 if ( ! function_exists( 'register_dev_network' ) ) {	
 	function register_dev_network( $args ) {
+		
+		$dev_networks = get_option( 'dev_networks' );
+		
+		if ( ! isset( $dev_networks[$args['name']] ) ) {
+			$dev_networks[] = $args['name'];
+			update_option( 'dev_networks' , $dev_networks );
+		}
+		
+		add_option( 'dev_network_'.$args['name'] , $args );
 
-
-			
 	}
 }
 
